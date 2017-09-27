371bb06190b8e08a92bc7859b57aeaa4a77aeee2
koddas <johan@idioti.se>
2017-09-27 11:22:46 +0200

    Added a directive for timestamp parsing

--- server-java/src/main/java/se/idioti/unicorns/server_java/Unicorn.java
+++ server-java/src/main/java/se/idioti/unicorns/server_java/Unicorn.java
@@ -2,6 +2,8 @@ package se.idioti.unicorns.server_java;
 
 import java.sql.Timestamp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 /**
  * A simple class representing a unicorn.
  * 
@@ -14,6 +16,7 @@ public class Unicorn {
 	public String description = "";
 	public String reportedBy = "";
 	public Location spottedWhere = new Location();
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
 	public Timestamp spottedWhen = new Timestamp(0);
 	public String image = "";
 	
